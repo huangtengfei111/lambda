561743f517bc2731a5e5b4696c2bbef94db2f846
htf <13619254401@163.com>
2020-11-21 23:35:50 +0800

    first commit

---
+++ src/test/java/com/example/demo/htf/entity/Student.java
@@ -0,0 +1,38 @@
+package com.example.demo.htf.entity;
+
+/**
+ * @Description: java类作用描述
+ * @Author: huangtf
+ * @CreateDate: 2020/11/21 22:03
+ */
+public class Student {
+
+    private String name;
+    private  Integer age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
---
+++ src/test/java/com/example/demo/htf/service/MyFunc.java
@@ -0,0 +1,7 @@
+package com.example.demo.htf.service;
+
+@FunctionalInterface
+public interface MyFunc {
+
+    public Integer getValue(Integer x);
+}
